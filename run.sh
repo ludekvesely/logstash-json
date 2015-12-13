c03930934322863dfc17bee85f601584a6db003e
Luděk Veselý <ludek.vesely@email.com>
2015-12-13 08:59:13 +0100

    escape slashes

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 if [ "$1" = 'logstash' ]; then
 	sed -i "s/ELASTICSEARCH_HOSTS/${ELASTICSEARCH_HOSTS}/g" /logstash.conf
 	sed -i "s/ELASTICSEARCH_USER/${ELASTICSEARCH_USER}/g" /logstash.conf
+	ELASTICSEARCH_PASSWORD=$(sed 's|/|\\/|g' <<< $ELASTICSEARCH_PASSWORD)
 	sed -i "s/ELASTICSEARCH_PASSWORD/${ELASTICSEARCH_PASSWORD}/g" /logstash.conf
 fi
 
